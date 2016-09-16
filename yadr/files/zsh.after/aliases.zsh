7e0188f03cd80b67e93f2a9d1eddfaeba75f5f57
Robert Roach <rjayroach@gmail.com>
2016-09-16 15:20:18 +0800

    general cleanup and re-organization

--- yadr/files/zsh.after/aliases.zsh
+++ yadr/files/zsh.after/aliases.zsh
@@ -1,18 +1,3 @@
-alias adc='spring rake app:db:clean'
-alias adm='spring rake app:db:migrate'
-alias admc='spring rake app:db:migrate:clean'
-alias admcs='spring rake app:db:migrate:seed'
-alias ads='spring rake app:db:seed'
-alias adsc='spring rake app:db:seed:clean'
-alias adsct='SEED_TENANT=true spring rake app:db:seed:clean'
-alias adtp='spring rake app:db:test:prepare'
-
-alias ap='ansible-playbook'
-alias apa='ansible-playbook /vagrant/ansible/provision.yml --tags alias && source ~/.zsh.after/aliases.zsh'
-alias app='ansible-playbook provision.yml -U vagrant -i hosts'
-alias apdb='ansible-playbook /vagrant/ansible/docker-build.yml --tags alias'
-
-alias beg='bundle exec guard'
 
 alias cv='cordova'
 alias cvb='cordova build'
@@ -30,61 +15,12 @@ alias dsh='dkr shell'
 alias dst='dkr stop'
 alias dstr='dkr stop -r'
 
-alias di='docker images'
-alias dps='docker ps'
 
-alias dc='spring rake db:clean'
-alias dm='spring rake db:migrate'
-alias dmc='spring rake db:migrate:clean'
-alias dmcs='spring rake db:migrate:seed'
-alias dsc='spring rake db:seed:clean'
-alias dtp='spring rake db:test:prepare'
-
-alias du='du -csh *'
-
-alias fs='foreman start -e <()'
-
-alias ggpl='spring stop; git checkout Gemfile.lock; git pull'
 alias gwdsc='bin/seed'
-
-alias gsu='git submodule update --remote --merge'
-
-alias pss='ps -ef|grep spring'
-
-alias rc='spring rails console'
-alias rg='spring rails generate'
-alias rgjc='spring rails generate jsonapi:controller'
-alias rgjr='spring rails generate jsonapi:resource'
-alias rgm='spring rails generate model'
-alias rk='spring rake'
-alias rkd='RAILS_ENV=test spring rake docs:generate'
-alias rs='spring rspec'
-
 alias seed='/home/vagrant/dev/play/platform/api-common/bin/seed.rb'
-
-alias src='spring stop; rc'
-alias ss='spring stop'
 alias supg='sudo su - postgres'
 
-alias tl='tmux list-sessions'
-alias ta='tmux a -dt $1'
-
-# Source and edit this file
-alias sa='source ~/.zsh.after/aliases.zsh'
-alias va='vi ~/.zsh.after/aliases.zsh'
-
-alias v='vagrant'
-alias vb='vagrant box'
-alias vd='vagrant destroy'
-alias vg='vagrant global-status'
-alias vh='vagrant halt'
-alias vp='vagrant provision'
-alias vs='vagrant ssh'
-alias vst='vagrant status'
-alias vu='vagrant up'
-alias vup='vagrant up --provision'
-alias vun='vagrant up /swarm-node-0[0-9]/'
-
+alias gsu='git submodule update --remote --merge'
 
 # NOTE: bes() only works if there is one line in the Procfile
 bes() {
